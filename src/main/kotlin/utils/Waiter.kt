a04e82cb011ff0a249221043504d3f1f0970619b
ilya-ukhanau <ilya.ukhanau@moneyman.ru>
2020-07-08 14:14:57 +0300

    Removee unused waiters.

--- src/main/kotlin/utils/Waiter.kt
+++ src/main/kotlin/utils/Waiter.kt
@@ -3,17 +3,13 @@ package utils
 import com.codeborne.selenide.Condition
 import com.codeborne.selenide.Selenide
 import com.codeborne.selenide.WebDriverRunner.getWebDriver
+import driver.config.DriverConfigProvider
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriverException
-import org.openqa.selenium.support.ui.ExpectedConditions
-import org.openqa.selenium.support.ui.FluentWait
 import org.openqa.selenium.support.ui.WebDriverWait
-import java.time.Duration
-import driver.config.DriverConfigProvider
 
 class Waiter {
   private val defaultTimeoutMilliseconds: Long = DriverConfigProvider().getSelenideDriverConfig().defaultTimeoutMilliseconds
-  private val defaultPollingInterval: Long = 500
 
   fun waitElementVisibleWithTimeout(element: By, timeoutMilliseconds: Long = defaultTimeoutMilliseconds) {
     Selenide.`$`(element).waitUntil(Condition.visible, timeoutMilliseconds)
@@ -21,25 +17,6 @@ class Waiter {
 
   fun waitElementEnabledWithTimeout(element: By, timeoutMilliseconds: Long = defaultTimeoutMilliseconds) {
     Selenide.`$`(element).waitUntil(Condition.enabled, timeoutMilliseconds)
-  }
-
-  fun waitExplicitlyForElement(element: By) {
-    WebDriverWait(getWebDriver(), defaultTimeoutMilliseconds).until(
-        ExpectedConditions.visibilityOfAllElementsLocatedBy(element))
-  }
-
-  fun waitFluentlyForElement(element: By) {
-    FluentWait(getWebDriver())
-        .withTimeout(Duration.ofSeconds(defaultTimeoutMilliseconds))
-        .pollingEvery(Duration.ofMillis(defaultPollingInterval))
-        .ignoring(WebDriverException::class.java)
-        .until(ExpectedConditions.visibilityOfAllElementsLocatedBy(element))
-  }
-
-  fun waitForJQuery() {
-    WebDriverWait(getWebDriver(), defaultTimeoutMilliseconds).until { webDriver ->
-      Selenide.executeJavaScript<Any>("return JQuery.active === 0") as Boolean
-    }
   }
 
   fun waitDomModelLoad(timeoutInSeconds: Long) {
@@ -59,22 +36,6 @@ class Waiter {
             }
           }
           isPageDomModelFullyLoaded()
-        }
-  }
-
-  fun waitElementNotExistsWithTimeout(elementName: String) {
-    var elementExists = false
-    WebDriverWait(getWebDriver(), defaultTimeoutMilliseconds)
-        .ignoring(WebDriverException::class.java)
-        .until { webDriver ->
-          fun isElementExistsInDom(): Boolean {
-            if (Selenide.executeJavaScript<Any>(
-                    "return !!document.getElementsByName('$elementName').length") as Boolean) {
-              elementExists = true
-            }
-            return elementExists
-          }
-          isElementExistsInDom()
         }
   }
 
