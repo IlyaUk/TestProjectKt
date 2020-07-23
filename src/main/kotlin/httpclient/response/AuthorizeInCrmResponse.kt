365a1e6b92879a088554f10f35a957c6142115d8
ilya-ukhanau <ilya.ukhanau@moneyman.ru>
2020-07-23 14:03:05 +0300

    Remove unnecessary inheritance from AuthorizeInCrmResponse data class.

--- src/main/kotlin/httpclient/response/AuthorizeInCrmResponse.kt
+++ src/main/kotlin/httpclient/response/AuthorizeInCrmResponse.kt
@@ -1,11 +1,9 @@
 package httpclient.response
 
-import httpclient.utils.SerializableRequest
-
 data class AuthorizeInCrmResponse(
     val id: Int,
     val userName: String,
     val localizedRole: String,
     val roleId: Int,
     val error: String?
-) : SerializableRequest
+)
