5791e97bdac505b2d999b047a4804bd9a66a522b
ilya-ukhanau <ilya.ukhanau@moneyman.ru>
2020-08-07 18:37:07 +0300

    Enable Login Test

--- core/src/test/kotlin/privatearea/LoginTest.kt
+++ core/src/test/kotlin/privatearea/LoginTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import services.PrivateAreaOperations
-@Disabled
+
 class LoginTest : BaseUiTest() {
   private val user = "u"
   private val pass = "p"
