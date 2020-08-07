8fb6a0ff93e1f671edf9cdea2ae41059a6b4f962
ilya-ukhanau <ilya.ukhanau@moneyman.ru>
2020-08-07 18:28:36 +0300

    Add gradle report generation stage to Jenkins file.

--- JenkinsfileSmokeTest.groovy
+++ JenkinsfileSmokeTest.groovy
@@ -3,7 +3,12 @@ pipeline {
   stages {
     stage('Git checkout') {
       steps {
-        checkout([$class: 'GitSCM', branches: [[name: '*/add-jenkins-pipeline']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/IlyaUk/TestProjectKt/']]])
+        checkout([$class: 'GitSCM',
+                  branches: [[name: '*/add-jenkins-pipeline']],
+                  doGenerateSubmoduleConfigurations: false,
+                  extensions: [],
+                  submoduleCfg: [],
+                  userRemoteConfigs: [[url: 'https://github.com/IlyaUk/TestProjectKt/']]])
       }
     }
     stage('Build project') {
@@ -18,7 +23,21 @@ pipeline {
     }
     stage('Allure report') {
       steps {
-        allure([includeProperties: false, jdk: '', results: [[path: 'core\\build\\allure-results']]])
+        allure([includeProperties: false,
+                jdk: '',
+                results: [[path: 'core\\build\\allure-results']]])
+      }
+    }
+    stage('Gradle report') {
+      steps {
+        publishHTML(
+            [allowMissing: false,
+             alwaysLinkToLastBuild: false,
+             keepAll: false,
+             reportDir: 'core\\build\\reports\\tests\\test',
+             reportFiles: 'index.html',
+             reportName: 'HTML Report - Gradle',
+             reportTitles: ''])
       }
     }
   }
